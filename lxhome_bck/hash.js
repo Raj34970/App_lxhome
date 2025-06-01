6810d1f58356af28777d883e8881345e9a3634db
debian <debian@nextcloud-home.lxhome.local>
2025-06-01 03:30:10 +0200

    removed bugs

--- lxhome_bck/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const password = 'Rajarshi2024@%';
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds, function(err, hash) {
-  if (err) throw err;
-  console.log('Hashed password:', hash);
-});
